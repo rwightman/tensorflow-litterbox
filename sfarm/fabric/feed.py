Add selectable image scaling, fix issues. * Add caffe style vs default image scaling via flag * Remove activation function from logits FC layer for resnet/vgg * Output multiple losses during train to see total vs logits output loss

sfarm/fabric/feed.py
@@ -13,6 +13,8 @@ tf.app.flags.DEFINE_integer('batch_size', 16,
                             """Number of images to process in a batch.""")
 tf.app.flags.DEFINE_integer('image_size', 299,
                             """Provide square images of this size.""")
+tf.app.flags.DEFINE_string('image_fmt', 'default',
+                            """Either 'default' RGB [-1,1] or 'caffe' BGR [0,255]""")
 tf.app.flags.DEFINE_integer('num_preprocess_threads', 4,
                             """Number of preprocessing threads per tower. """
                             """Please make this a multiple of 4.""")
@@ -66,6 +68,7 @@ class Feed(object):
         self.height = FLAGS.image_size
         self.width = FLAGS.image_size
         self.depth = 3
+        self.caffe_fmt = True if FLAGS.image_fmt == 'caffe' else False
 
     def num_batches_per_epoch(self):
         return self.dataset.num_examples_per_epoch() / self.batch_size
@@ -198,7 +201,8 @@ class Feed(object):
             # Parse a serialized Example proto to extract the image and metadata.
             image_buffer, label_index, bbox, _, filename = self.proto_parser(example_serialized)
             tf.Print(image_buffer, [label_index])
-            image = self.image_preprocess(image_buffer, self.height, self.width, bbox, train, thread_id)
+            image = self.image_preprocess(
+                image_buffer, self.height, self.width, bbox, self.caffe_fmt, train, thread_id)
             inputs.append([image, label_index, filename])
 
         return inputs
@@ -248,7 +252,7 @@ class Feed(object):
             image_buffer = tf.read_file(filename)
             image = self.image_preprocess(
                 image_buffer,
-                height=self.height, width=self.width,
+                height=self.height, width=self.width, caffe_fmt=self.caffe_fmt,
                 train=train, thread_id=thread_id)
             inputs.append([image, label_index, filename])
 

sfarm/fabric/image_processing.py
@@ -42,6 +42,7 @@ from __future__ import print_function
 
 import tensorflow as tf
 
+IMAGENET_MEAN = [103.939, 116.779, 123.68]
 
 def decode_jpeg(image_buffer, depth=3, scope=None):
     """Decode a JPEG string into one 3-D float image Tensor.
@@ -199,7 +200,7 @@ def eval_image(image, height, width, scope=None):
         return image
 
 
-def image_preprocess(image_buffer, height, width, bbox=None, train=False, thread_id=0):
+def image_preprocess(image_buffer, height, width, bbox=None, caffe_fmt=False, train=False, thread_id=0):
     """Decode and preprocess one image for evaluation or training.
 
     Args:
@@ -228,7 +229,19 @@ def image_preprocess(image_buffer, height, width, bbox=None, train=False, thread
     else:
         image = eval_image(image, height, width)
 
-    # Finally, rescale to [-1,1] instead of [0, 1)
-    image = tf.sub(image, 0.5)
-    image = tf.mul(image, 2.0)
+    if caffe_fmt:
+        # Rescale to [0, 255]
+        image = tf.mul(image, 255.0)
+        # Convert RGB to BGR
+        red, green, blue = tf.split(3, 3, image)
+        image = tf.concat(3, [
+            blue - IMAGENET_MEAN[0],
+            green - IMAGENET_MEAN[1],
+            red - IMAGENET_MEAN[2],
+            ])
+    else:
+        # Finally, rescale to [-1,1] instead of [0, 1)
+        image = tf.sub(image, 0.5)
+        image = tf.mul(image, 2.0)
+
     return image

sfarm/fabric/train.py
@@ -27,7 +27,7 @@ import re
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.contrib import losses
+#from tensorflow.contrib import losses
 
 from .image_processing import *
 from .feed import Feed
@@ -132,6 +132,7 @@ def _tower_loss(images, labels, num_classes, model, scope):
         # Remove 'tower_[0-9]/' from the name in case this is a multi-GPU training
         # session. This helps the clarity of presentation on TensorBoard.
         loss_name = re.sub('%s_[0-9]*/' % model.TOWER_NAME, '', l.op.name)
+        print(loss_name)
         # Name each loss as '(raw)' and name the moving average version of the loss
         # as the original loss name.
         tf.scalar_summary(loss_name + ' (raw)', l)
@@ -139,8 +140,9 @@ def _tower_loss(images, labels, num_classes, model, scope):
 
     with tf.control_dependencies([loss_averages_op]):
         total_loss = tf.identity(total_loss)
+        output_loss = tf.identity(tower_losses[0])
 
-    return total_loss
+    return total_loss, output_loss
 
 
 def _average_gradients(tower_grads):
@@ -207,20 +209,18 @@ def train(dataset, model):
                                         staircase=True)
 
         # Create an optimizer that performs gradient descent.
-        opt = tf.train.RMSPropOptimizer(lr, RMSPROP_DECAY,
-                                        momentum=RMSPROP_MOMENTUM,
-                                        epsilon=RMSPROP_EPSILON)
+        #opt = tf.train.RMSPropOptimizer(lr, RMSPROP_DECAY,
+        #                                momentum=RMSPROP_MOMENTUM,
+        #                                epsilon=RMSPROP_EPSILON)
 
         #opt = tf.train.AdadeltaOptimizer(lr, epsilon=1e-6)
 
         #opt = tf.train.AdamOptimizer(lr, epsilon=.01)
 
-        #opt = tf.train.MomentumOptimizer(lr, 0.9)
+        opt = tf.train.MomentumOptimizer(lr, 0.9)
 
         # Get images and labels for ImageNet and split the batch across GPUs.
-        assert FLAGS.batch_size % FLAGS.num_gpus == 0, (
-            'Batch size must be divisible by number of GPUs')
-        split_batch_size = int(FLAGS.batch_size / FLAGS.num_gpus)
+        assert FLAGS.batch_size % FLAGS.num_gpus == 0, 'Batch size must be divisible by number of GPUs'
 
         images, labels, _ = feed.distorted_inputs()
 
@@ -246,15 +246,15 @@ def train(dataset, model):
                             # Calculate the loss for one tower of the ImageNet model. This
                             # function constructs the entire ImageNet model but shares the
                             # variables across all towers.
-                            loss = _tower_loss(images_splits[i], labels_splits[i], num_classes, model, scope)
+                            tower_losses = _tower_loss(images_splits[i], labels_splits[i], num_classes, model, scope)
 
                         # Reuse variables for the next tower.
                         tf.get_variable_scope().reuse_variables()
                     else:
-                        loss = _tower_loss(images_splits[i], labels_splits[i], num_classes, model, scope)
+                        tower_losses = _tower_loss(images_splits[i], labels_splits[i], num_classes, model, scope)
 
                     # Calculate the gradients for the batch of data on this ImageNet tower.
-                    grads = opt.compute_gradients(loss)
+                    grads = opt.compute_gradients(tower_losses[0])
 
                     # Keep track of the gradients across all towers.
                     tower_grads.append(grads)
@@ -338,15 +338,15 @@ def train(dataset, model):
 
         for step in range(FLAGS.max_steps):
             start_time = time.time()
-            _, loss_value = sess.run([train_op, loss])
+            _, loss_values = sess.run([train_op, tower_losses])
             duration = time.time() - start_time
 
-            assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+            assert not np.isnan(loss_values[0]), 'Model diverged with loss = NaN'
 
             if step % 10 == 0:
                 examples_per_sec = FLAGS.batch_size / float(duration)
-                format_str = '%s: step %d, loss = %.2f (%.1f examples/sec; %.3f sec/batch)'
-                print(format_str % (datetime.now(), step, loss_value, examples_per_sec, duration))
+                format_str = '%s: step %d, total loss = %.2f output loss = %.4f (%.1f examples/sec; %.3f sec/batch)'
+                print(format_str % (datetime.now(), step, loss_values[0], loss_values[1], examples_per_sec, duration))
 
             if step % 100 == 0:
                 summary_str = sess.run(summary_op)

sfarm/resnet/model_resnet.py
@@ -9,9 +9,6 @@ from tensorflow.python.ops import math_ops
 
 FLAGS = tf.app.flags.FLAGS
 
-VGG_MEAN = [103.939, 116.779, 123.68]
-
-
 #@layers.add_arg_scope
 def block(net, num_filters_internal, block_stride, bottleneck=True, activation_fn=tf.nn.relu):
 
@@ -95,7 +92,7 @@ def build_resnet(
                 endpoints['avg_pool'] = net
 
                 with tf.variable_scope('logits'):
-                    logits = layers.fully_connected(net, num_classes, scope='logits') #restore=restore_logits
+                    logits = layers.fully_connected(net, num_classes, activation_fn=None, scope='logits') #restore=restore_logits
                     # 1 x 1 x num_classes
                     endpoints['logits'] = logits
                     endpoints['predictions'] = tf.nn.softmax(logits, name='predictions')
@@ -111,18 +108,17 @@ class ModelResnet(model.Model):
     def __init__(self):
         super(ModelResnet, self).__init__()
 
-    # Input should be an rgb image [batch, height, width, 3]
-    # values scaled [0, 1]
-    def build(self, inputs_rgb, num_classes, variant='34', is_training=False, restore_logits=True, scope=None):
+    def build(self, inputs, num_classes, variant='34', is_training=False, restore_logits=True, scope=None):
 
-        #[18]  = {{2, 2, 2, 2}, 512, basicblock},
-        #[34]  = {{3, 4, 6, 3}, 512, basicblock},
-        #[50]  = {{3, 4, 6, 3}, 2048, bottleneck},
-        #[101] = {{3, 4, 23, 3}, 2048, bottleneck},
-        #[152] = {{3, 8, 36, 3}, 2048, bottleneck},
+        # [layer] config params
+        # [18]  = {{2, 2, 2, 2}, 512, basic},
+        # [34]  = {{3, 4, 6, 3}, 512, basic},
+        # [50]  = {{3, 4, 6, 3}, 2048, bottleneck},
+        # [101] = {{3, 4, 23, 3}, 2048, bottleneck},
+        # [152] = {{3, 8, 36, 3}, 2048, bottleneck},
 
-        #34 layer
-        bottleneck=False
+        # 34 layer config
+        bottleneck=False  # basic
         num_blocks = [3, 4, 6, 3]
 
         batch_norm_params = {
@@ -144,7 +140,7 @@ class ModelResnet(model.Model):
                     normalizer_params=batch_norm_params):
 
                 logits, endpoints = build_resnet(
-                    inputs_rgb,
+                    inputs,
                     num_classes=num_classes,
                     num_blocks=num_blocks,
                     bottleneck=bottleneck,
@@ -159,7 +155,7 @@ class ModelResnet(model.Model):
         )
 
         # Add summaries for viewing model statistics on TensorBoard.
-        #self.activation_summaries()
+        self.activation_summaries()
 
         return logits, None
 

sfarm/vgg/model_vgg16.py
@@ -9,7 +9,6 @@ from tensorflow.python.ops import math_ops
 
 FLAGS = tf.app.flags.FLAGS
 
-IMAGENET_MEAN = [103.939, 116.779, 123.68]
 
 def build_vgg16(
         inputs,
@@ -75,7 +74,7 @@ def build_vgg16(
                 endpoints['fc7'] = net
 
                 with tf.variable_scope('logits'):
-                    logits = layers.fully_connected(net, num_classes, scope='logits') #restore=restore_logits
+                    logits = layers.fully_connected(net, num_classes, activation_fn=None, scope='logits') #restore=restore_logits
                     # 1 x 1 x num_classes
                     endpoints['logits'] = logits
                     endpoints['predictions'] = tf.nn.softmax(logits, name='predictions')
@@ -93,20 +92,7 @@ class ModelVgg16(model.Model):
 
     # Input should be an rgb image [batch, height, width, 3]
     # values scaled [0, 1]
-    def build(self, inputs_rgb, num_classes, is_training=False, restore_logits=True, scope=None):
-
-        # rgb_scaled = inputs_rgb * 255.0
-
-        # # Convert RGB to BGR
-        # red, green, blue = tf.split(3, 3, rgb_scaled)
-        # assert red.get_shape().as_list()[1:] == [224, 224, 1]
-        # assert green.get_shape().as_list()[1:] == [224, 224, 1]
-        # assert blue.get_shape().as_list()[1:] == [224, 224, 1]
-        # inputs_bgr = tf.concat(3, [
-        #     blue - IMAGENET_MEAN[0],
-        #     green - IMAGENET_MEAN[1],
-        #     red - IMAGENET_MEAN[2],
-        # ])
+    def build(self, inputs, num_classes, is_training=False, restore_logits=True, scope=None):
 
         batch_norm_params = {
             # Decay for the moving averages.
@@ -114,20 +100,22 @@ class ModelVgg16(model.Model):
             # epsilon to prevent 0s in variance.
             'epsilon': 0.001,
         }
-        l2_regularizer = None #layers.l2_regularizer(0.0004) #0.00004
+        l2_regularizer = layers.l2_regularizer(0.0005) #0.00004
 
         with arg_scope(
                 [layers.conv2d, layers.fully_connected],
-                weights_initializer=layers.xavier_initializer(),
+                weights_initializer=layers.variance_scaling_initializer(factor=.5),
                 weights_regularizer=l2_regularizer,
-                activation_fn=tf.nn.relu):
+                activation_fn=tf.nn.relu
+        ):
             with arg_scope(
                     [layers.conv2d],
                     normalizer_fn=layers.batch_norm,
-                    normalizer_params=batch_norm_params):
+                    normalizer_params=batch_norm_params
+            ):
 
                 logits, endpoints = build_vgg16(
-                    inputs_rgb,
+                    inputs,
                     num_classes=num_classes,
                     is_training=is_training,
                     restore_logits=restore_logits,