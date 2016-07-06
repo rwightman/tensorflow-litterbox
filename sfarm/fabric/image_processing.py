VGG training successfully with adadelta optimizer. * Fix error in Caffe scaling * Disable batch norm for VGG

sfarm/fabric/image_processing.py
@@ -233,8 +233,8 @@ def image_preprocess(image_buffer, height, width, bbox=None, caffe_fmt=False, tr
         # Rescale to [0, 255]
         image = tf.mul(image, 255.0)
         # Convert RGB to BGR
-        red, green, blue = tf.split(3, 3, image)
-        image = tf.concat(3, [
+        red, green, blue = tf.split(2, 3, image)
+        image = tf.concat(2, [
             blue - IMAGENET_MEAN[0],
             green - IMAGENET_MEAN[1],
             red - IMAGENET_MEAN[2],

sfarm/vgg/model_vgg16.py
@@ -111,8 +111,8 @@ class ModelVgg16(model.Model):
         ):
             with arg_scope(
                     [layers.conv2d],
-                    normalizer_fn=layers.batch_norm,
-                    normalizer_params=batch_norm_params,
+                    #normalizer_fn=layers.batch_norm,
+                    #normalizer_params=batch_norm_params,
                     weights_initializer=layers.variance_scaling_initializer(factor=1.0),
                     weights_regularizer=l2_regularizer,
                     activation_fn=tf.nn.relu