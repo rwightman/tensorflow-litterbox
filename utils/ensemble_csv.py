e1e99c1007910cfe747c6565d544d699a7770842
Ross Wightman <rwightman@gmail.com>
2016-09-08 22:48:07 -0700

    Add LICENSE. Remove/update some util scripts. * Remove some outdated sfarm specific util scrips. * Update ensemble script the newer ver from Torch repo

--- utils/ensemble_csv.py
+++ utils/ensemble_csv.py
@@ -1,11 +1,3 @@
-# Copyright (C) 2016 Ross Wightman. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-# ==============================================================================
 import argparse
 import numpy as np
 import pandas as pd
@@ -16,6 +8,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', '--outfile', metavar='out_file', type=str, default='ensemble.csv')
     parser.add_argument('-w', '--weights', metavar='weight_file', type=str, default='')
+    parser.add_argument('-m', '--mean', type=str, default='arith')
     parser.add_argument('files', metavar='filename', type=str, nargs='*')
     args = parser.parse_args()
     args = vars(args)
@@ -23,8 +16,8 @@ def main():
     files = args['files']
     outfile = args['outfile']
     weights_file = args['weights']
+    mean_type = args['mean']
 
-    # use files in weights csv if it is defined, otherwise use filenames from positional cmd line args
     if weights_file:
         wf = pd.read_csv(weights_file, names=['file', 'weight'], header=None, index_col=False)
         files, weights = wf.file.tolist(), wf.weight.tolist()
@@ -35,13 +28,23 @@ def main():
     frames = []
     for file, weight in zip(files, weights):
         df = pd.read_csv(file, header=0, index_col=False)
-        df.iloc[:, 1:] = df.iloc[:, 1:] * weight
+        if mean_type == 'geom':
+            df.iloc[:, 1:] = df.iloc[:, 1:].pow(weight)
+        else:
+            df.iloc[:, 1:] = df.iloc[:, 1:] * weight
         frames.append(df)
+    
     merged = pd.concat(frames)
-    # group by first column which is assumed to be the image name/id across all models
-    avg = merged.groupby(merged.iloc[:, 0]).sum()
-    avg = avg / sum(weights)
-    avg.to_csv(outfile)
+    weights_sum = sum(weights)
+    if mean_type == 'geom':
+        result = merged.groupby(merged.Img).prod()
+        result = result.pow(1/weights_sum)
+        result = result.div(result.sum(axis=1), axis=0)
+    else:
+        result = merged.groupby(merged.Img).sum()
+        result = result / weights_sum
+    
+    result.to_csv(outfile)
 
 if __name__ == "__main__":
     main()
