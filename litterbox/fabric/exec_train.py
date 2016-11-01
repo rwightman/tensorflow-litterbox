Numerous tweaks and fixes to improve model training. * Scope fixes to allow loading of pretrained google inception-resnet-v2 weights. * Add weighting to huber loss, decrease weight of aux loss * XYZ dim > 1 regression var seems to work with loss, needs verification

litterbox/fabric/exec_train.py
@@ -234,7 +234,7 @@ def _build_train_graph(feed, model):
     # Add histograms for gradients.
     for grad, var in grads:
         if grad is not None:
-            summaries.append(tf.histogram_summary(var.op.name + '/gradients', grad))
+            summaries.append(tf.histogram_summary(model.strip_common_scope(var.op.name) + '/gradients', grad))
 
     update_ops = []
 
@@ -304,11 +304,11 @@ def train(dataset, processor, model):
         # When fine-tuning a model, we do not restore the logits but instead we
         # randomly initialize the logits. The number of classes in the output of the
         # logit is the number of classes in specified Dataset.
-        restore_logits = not FLAGS.fine_tune
+        restore_outputs = not FLAGS.fine_tune
 
         if FLAGS.pretrained_model_path:
             assert tf.gfile.Exists(FLAGS.pretrained_model_path)
-            variables_to_restore = model.variables_to_restore(restore_logits)
+            variables_to_restore = model.variables_to_restore(restore_outputs)
             if tf.gfile.IsDirectory(FLAGS.pretrained_model_path):
                 model_path = tf.train.latest_checkpoint(FLAGS.pretrained_model_path)
             else:

litterbox/fabric/image_processing_common.py
@@ -122,7 +122,8 @@ def distort_affine_cv2(image, alpha_affine=10, random_state=None):
     pts2 = pts1 + random_state.uniform(-alpha_affine, alpha_affine, size=pts1.shape).astype(np.float32)
 
     M = cv2.getAffineTransform(pts1, pts2)
-    distorted_image = cv2.warpAffine(image, M, shape_size[::-1], borderMode=cv2.BORDER_REFLECT_101)
+    distorted_image = cv2.warpAffine(
+        image, M, shape_size[::-1], borderMode=cv2.BORDER_REPLICATE) #cv2.BORDER_REFLECT_101)
 
     return distorted_image
 

litterbox/fabric/loss.py
@@ -25,7 +25,7 @@ def loss_softmax_cross_entropy_with_aux(logits, labels, aux_logits=None):
             aux_logits, dense_labels, label_smoothing=0.1, weight=0.4, scope='aux_loss')
 
 
-def loss_huber(predictions, targets, delta=1.0, scope=None):
+def loss_huber(predictions, targets, delta=1.0, weight=1.0, scope=None):
     with tf.name_scope(scope, "huber_loss", [predictions, targets]):
         predictions.get_shape().assert_is_compatible_with(targets.get_shape())
         predictions = tf.to_float(predictions)
@@ -41,10 +41,10 @@ def loss_huber(predictions, targets, delta=1.0, scope=None):
         right_opt = delta * diff_abs - delta_fact
         losses_val = tf.select(condition, left_opt, right_opt)
 
-        return tf.contrib.losses.compute_weighted_loss(losses_val)
+        return tf.contrib.losses.compute_weighted_loss(losses_val, weight=weight)
 
 
-def loss_huber_with_aux(predictions, targets, delta=1.0, aux_predictions=None):
-    loss_huber(predictions, targets, delta=delta)
+def loss_huber_with_aux(predictions, targets, delta=1.0, weight=1.0, aux_predictions=None):
+    loss_huber(predictions, targets, delta=delta, weight=weight)
     if aux_predictions is not None:
-        loss_huber(aux_predictions, targets, delta=delta, scope='aux_huber_loss')
+        loss_huber(aux_predictions, targets, delta=delta, weight=weight*0.4, scope='aux_huber_loss')

litterbox/models/sdc/build_inception_resnet_sdc.py
@@ -66,7 +66,7 @@ def build_inception_resnet_sdc_regression_v1(
         output_cfg={'steer': 1},
         is_training=True,
         bayesian=False,
-        dropout_keep_prob=0.8,
+        dropout_keep_prob=0.5,
         reuse=None,
         scope='InceptionResnetV2'):
     """Creates the Inception Resnet V2 model.
@@ -215,6 +215,7 @@ def build_inception_resnet_sdc_regression_v1(
         with tf.variable_scope('Output'):
             net = slim.avg_pool2d(net, net.get_shape()[1:3], padding='VALID', scope='AvgPool_8x8')
             net = slim.flatten(net)
+            net = slim.fully_connected(net, 2048, scope='Fc1')
             net = slim.dropout(net, dropout_keep_prob, is_training=bayesian or is_training, scope='Dropout')
 
             output = {}

litterbox/models/sdc/model_sdc.py
@@ -30,6 +30,8 @@ class ModelSdc(fabric.model.Model):
         super(ModelSdc, self).__init__()
         params = fabric.model.merge_params(sdc_default_params, params)
         self.output_cfg = params['outputs']
+        # model variable scope needs to match google net for pretrained weight compat
+        self.model_scope = "InceptionResnetV2"
 
     def build_tower(self, inputs, is_training=False, scope=None):
 
@@ -37,12 +39,7 @@ class ModelSdc(fabric.model.Model):
             output, endpoints = build_inception_resnet_sdc_regression_v1(
                 inputs,
                 self.output_cfg,
-                is_training=is_training,
-                scope=scope)
-
-        #scope_search = re.search('%s_[0-9]*/(\w+)/' % self.TOWER_PREFIX, output['steer'].op.name)
-        #print(scope_search)
-        #self.model_scope = "InceptionResnetV2"
+                is_training=is_training)
 
         aux_output = None
         if 'AuxOutput' in endpoints:
@@ -68,9 +65,15 @@ class ModelSdc(fabric.model.Model):
                 tower.outputs['xyz'], targets[1], aux_predictions=tower.aux_outputs['xyz'])
         if 'steer' in self.output_cfg:
             targets_steer = tf.expand_dims(targets[0], 1)
-            print(tower.outputs['steer'].get_shape(), targets_steer.get_shape())
             fabric.loss.loss_huber_with_aux(
                 tower.outputs['steer'], targets_steer, aux_predictions=tower.aux_outputs['steer'])
+
+    def output_scopes(self):
+        rel_scopes = ['Logits', 'Output/OutputXYZ', 'Output/OutputSteer', 'Output/Fc1',
+                      'AuxLogits/OutputXYZ', 'AuxLogits/OutputSteer', 'AuxLogits/Fc1']
+        abs_scopes = [self.model_scope + '/' + x for x in rel_scopes]
+        #abs_scopes = ['[\w]*/' + x for x in rel_scopes]
+        return abs_scopes
 
     @staticmethod
     def eval_loss_op(predictions, targets):